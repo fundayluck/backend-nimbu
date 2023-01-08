93851518940afbfc509bd0555aea335add3db16f
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-01-08 15:06:07 +0700

    test 10

--- config/cors.js
+++ config/cors.js
@@ -1,8 +1,8 @@
-const whitelist = ['http://127.0.0.1:3000']
+const whitelist = []
 const corsOptions = {
     origin: (origin, callback) => {
-        console.log(origin);
         if (whitelist.indexOf(origin) !== -1) {
+
             callback(null, true)
         } else {
             callback(new Error())
