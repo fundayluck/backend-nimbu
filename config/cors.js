cbd046f8afaae98c40c2f2341b2ad980066344a4
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-01-08 14:47:00 +0700

    change callback

--- config/cors.js
+++ config/cors.js
@@ -6,7 +6,7 @@ const corsOptions = {
         if (whitelist.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
-            callback(new Error('not allowed by CORS'))
+            callback(new Error())
         }
     }
 }
